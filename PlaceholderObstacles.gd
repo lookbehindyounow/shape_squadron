87673be3a2458bb3f4dc4eb082e1958c8238bba9
Kieron Harrower <somebody.else.2017@gmail.com>
2023-10-09 11:20:06 +0100

    got collisions working with placeholder obstacles, the flying triangle in place of a fighter jet just slides around them for now

---
+++ PlaceholderObstacles.gd
@@ -0,0 +1,15 @@
+extends StaticBody3D
+
+func _ready():
+	$MultiMeshInstance3d.multimesh.instance_count=100
+	for x in range(10):
+		for z in range(10):
+			var pos=Transform3D().translated(Vector3(5*x,2.5,5*z))
+			$MultiMeshInstance3d.multimesh.set_instance_transform(10*x+z,pos)
+
+			var collision_shape=CollisionShape3D.new()
+			var shape=BoxShape3D.new()
+			shape.size=(Vector3(1,5,1))
+			collision_shape.set_shape(shape)
+			collision_shape.set_transform(pos)
+			add_child(collision_shape)
