71bdf956f74908bb6d1c006fd6129bc1301b2136
Kieron Harrower <somebody.else.2017@gmail.com>
2023-10-12 08:26:56 +0100

    bullet intercept icons are now only instantiated if they'll be within the HUD circle

--- UI.gd
+++ UI.gd
@@ -24,28 +24,18 @@ func _process(delta):
 	icons=[]
 	for i in range(HUD_points.size()):
 		icons.append([icon_scene.instantiate()])
-		add_child(icons[i][0])
-		if HUD_points[i][2]:
-			icons[i].append(icon_scene.instantiate())
-			icons[i][1].set_type("ahead")
-			add_child(icons[i][1])
-		
-	for i in range(HUD_points.size()):
-		var prev_point_radius=(icons[i][0].position-Vector2(screen_dimensions/2)).length()/(screen_dimensions.y/2.2)
 		# angle between top of HUD circle & forward is 0.65 rad
 		var point_radius=min(1,HUD_points[i][0][1]/0.65)
-		if point_radius<1 && prev_point_radius>0.999:
+		icons[i][0].position=Vector2(screen_dimensions/2)+((point_radius*screen_dimensions.y/2.2)*Vector2.UP.rotated(HUD_points[i][0][0]))
+		if point_radius<1:
 			icons[i][0].set_type("hollow")
-		elif point_radius==1 && prev_point_radius<0.999:
-			icons[i][0].set_type("full")
 		if not HUD_points[i][2]:
 			icons[i][0].set_colour("#00f")
-		
-		icons[i][0].position=Vector2(screen_dimensions/2)+((point_radius*screen_dimensions.y/2.2)*Vector2.UP.rotated(HUD_points[i][0][0]))
+		add_child(icons[i][0])
 		
 		if HUD_points[i][2]:
 			if HUD_points[i][1][1]<0.65:
+				icons[i].append(icon_scene.instantiate())
 				icons[i][1].position=Vector2(screen_dimensions/2)+(((HUD_points[i][1][1]/0.65)*screen_dimensions.y/2.2)*Vector2.UP.rotated(HUD_points[i][1][0]))
-				icons[i][1].show()
-			else:
-				icons[i][1].hide()
+				icons[i][1].set_type("ahead")
+				add_child(icons[i][1])
